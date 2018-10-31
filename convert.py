2650cd08663cbf1c0563642c4c150e5609ae143f
Peder Bergebakken Sundt <pbsds@hotmail.com>
2018-10-31 18:57:25 +0100

    Implement a MIDI to SongEvent converter

---
+++ convert.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import mido, sys
+from collections import namedtuple
+
+
+SongEvent = namedtuple("NameTuple", "t, channel, target")
+# target is half of SAMPLERATE/note freq
+
+SAMPLERATE = 44100
+
+def note2freq(n):
+	return 440*2**((n-57)/12)
+def note2target(n):
+	return int(SAMPLERATE / ( note2freq(n) * 2 ) + 0.698)
+
+def convert_mido_midi_to_song_events(mid):
+	midi_state = { # midi_state_2_song_event_channel
+		channel : {}
+		for channel in range(16)
+	}
+	song_event_channel = set() # taken or not
+	
+	t = 0
+	for msg in mid:
+		if msg.time > 0:
+			t += msg.time*SAMPLERATE
+		if hasattr(msg, "channel") and msg.channel == 9:
+			continue # drums
+			
+		if msg.type == "note_on" and msg.velocity:
+			if msg.note in midi_state[msg.channel]:
+				channel = midi_state[msg.channel][msg.note]
+			else:
+				channel = 0
+				while channel in song_event_channel:
+					channel += 1
+				midi_state[msg.channel][msg.note] = channel
+				song_event_channel.add(channel)
+			
+			yield SongEvent(int(t), channel, note2target(msg.note))
+			t -= int(t)
+		elif msg.type == "note_off" \
+		  or msg.type == "note_on" and msg.velocity == 0:
+			channel = midi_state[msg.channel][msg.note]
+			del midi_state[msg.channel][msg.note]
+			song_event_channel.remove(channel)
+			yield SongEvent(int(t), channel, 0)
+			t -= int(t)
+		elif msg.type == "pitchwheel":
+			#msg.channel
+			#float(msg.pitch)/8192.
+			pass
+
+
+def song_events_to_c(events, name):
+	return "\n".join([
+		"#include <stdint.h>",
+		"#include \"player.h\"",
+		"// {wait time in samples, channel index, target}",
+
+		f"static const SongEvent {name}[] = {{",
+		*(f"\t{{{t:>7}, {channel:>3}, {target:>4}}},"
+			for t, channel, target in events),
+		"\t{0, 999999, 0} // end of song",
+		"};",
+	])
+
+mid = mido.MidiFile(sys.argv[1])
+
+events = convert_mido_midi_to_song_events(mid)
+print(song_events_to_c(events, "my_song"))
